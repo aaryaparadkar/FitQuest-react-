ca9720f38fdfaed2c448ec7cace4e2ef79ea4cc0
vishav-pathania <vishavpathania40@gmail.com>
2024-02-27 23:39:10 +0530

    setup for client

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
+
+/*<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* Add more routes as needed }
+        </Routes>
+        </BrowserRouter>
+        */ 
