c674e424f18e825e76235bb45411d246c2493d55
Aarya Paradkar <aaru7276@gmail.com>
2024-02-28 01:44:42 +0530

    landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
   return (
-    
+    <div>
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
   );
 }
 
